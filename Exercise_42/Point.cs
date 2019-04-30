0e8454a59d5fe97e51b1f1482f2964b0fa68e47d
Jacob <jacobashin@gmail.com>
2019-04-30 00:08:08 -0400

    FirstCommit

---
+++ Exercise_42/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_42
+{
+   public class Point
+    {
+        private int x;
+
+        private int y;
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+    }
+}
---
+++ Exercise_42/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a 'x' coordinate");
+
+            var userInput1 = Console.ReadLine();
+
+            Point point = new Point();
+
+            int.TryParse(userInput1, out int x);
+
+           point.X = x;
+
+            Console.WriteLine("Please enter a 'y' coordinate");
+
+            var userInput2 = Console.ReadLine();
+
+            int.TryParse(userInput2, out int y);
+
+            point.Y = y;
+
+            Console.WriteLine($"{point.X}, {point.Y} ");
+
+            Console.ReadLine();
+
+        }
+    }
+}
